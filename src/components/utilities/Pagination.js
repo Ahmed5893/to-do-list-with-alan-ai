8a4492061647de0ddd5d20b28a98bf330f6d4cb4
ahmed <ahmed5893hneynia@gmail.com>
2023-01-19 14:27:06 +0100

    commit

--- src/components/utilities/Pagination.js
+++ src/components/utilities/Pagination.js
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Pagination = ({todoPerPage,totalTodos,paginate}) => {
-    const pageNumbers=[];
-    for(let i=1; i<=Math.ceil(totalTodos/todoPerPage); i++){
+const Pagination = ({ todoPerPage, totalTodos, paginate }) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalTodos / todoPerPage); i++) {
     pageNumbers.push(i);
-    }
+  }
   return (
-    <nav className="position-absolute top-100 start-50 translate-middle pb-5" >
-    <ul className='pagination'>
-      {pageNumbers.map(number => (
-        <li key={number} className='page-item'>
-          <button onClick={() =>paginate(number)} className='page-link'>
-            {number}
-          </button>
-        </li>
-      ))}
-    </ul>
-  </nav>
-  )
-}
+    <nav className="position-absolute top-100 start-50 translate-middle pb-5">
+      <ul className="pagination">
+        {pageNumbers.map((number) => (
+          <li key={number} className="page-item">
+            <button onClick={() => paginate(number)} className="page-link">
+              {number}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-export default Pagination
+export default Pagination;
