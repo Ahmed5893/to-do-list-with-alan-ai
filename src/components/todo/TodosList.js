e547938faf2ddb19c1c4ace3c64591cc36e5da18
ahmed <ahmed5893hneynia@gmail.com>
2023-02-12 17:01:21 +0100

    fix alan ai edit and delete

--- src/components/todo/TodosList.js
+++ src/components/todo/TodosList.js
@@ -24,11 +24,36 @@ const TodosList = () => {
     alanBtn({
       key: "21fdb362d25080992220db42caa44f712e956eca572e1d8b807a3e2338fdd0dc/stage",
       onCommand: (commandData) => {
-        dispatch({ type: "ADD", task: commandData.data.toString() });
+        console.log("first", commandData);
+
+        switch (commandData.command) {
+          case "ADD":
+            dispatch({ type: "ADD", task: commandData.data.toString() });
+            break;
+          case "REMOVE":
+            const index = todos.findIndex(
+              (el) => el.task === commandData.data.toString()
+            );
+            const id = todos[index].id;
+            dispatch({ type: "REMOVE", id: id });
+            break;
+          case "EDIT":
+            const i = todos.findIndex(
+              (el) => el.task === commandData.data.item.toString()
+            );
+            console.log(i, "read that");
+            const x = todos[i].id;
+            const newItem=commandData.data.new.toString()
+           console.log('item', newItem);
+            dispatch({
+              type: "EDIT",id:x,taskModified:newItem});
+            break;
+          default:
+            break;
+        }
       },
     });
-     
-  }, [dispatch]);
+  }, [dispatch, todos]);
   return (
     <div className={classes.list}>
       {currentTodo.map((todo) => (
